1f44c0d12337a668361f57b57d83dbbcc60a3d1d
Ganghyungoo <104668071+Ganghyungoo@users.noreply.github.com>
2024-03-26 17:57:10 +0900

    Feat add fir group screen connect
    피트그룹 화면 mock 데이터 및 화면 연결 완료

---
+++ app/src/main/java/com/fitmate/fitmate/presentation/ui/mygroup/list/adapter/MyFitGroupAdapter.kt
@@ -0,0 +1,44 @@
+package com.fitmate.fitmate.ui.myfit.list.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import com.fitmate.fitmate.databinding.ItemGroupBinding
+import com.fitmate.fitmate.domain.model.FitGroup
+import com.fitmate.fitmate.ui.myfit.list.MyFitGroupViewHolder
+
+class MyFitGroupAdapter(private val itemClick:(FitGroup) -> Unit) : ListAdapter<FitGroup, MyFitGroupViewHolder>(diffUtil) {
+
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int,
+    ): MyFitGroupViewHolder {
+        return MyFitGroupViewHolder(ItemGroupBinding.inflate(LayoutInflater.from(parent.context),parent,false),itemClick)
+    }
+
+    override fun onBindViewHolder(holder: MyFitGroupViewHolder, position: Int) {
+        holder.binding(currentList[position])
+    }
+
+    companion object {
+        private val diffUtil = object: DiffUtil.ItemCallback<FitGroup>() {
+            override fun areItemsTheSame(
+                oldItem: FitGroup,
+                newItem: FitGroup,
+            ): Boolean {
+                return oldItem == newItem
+            }
+
+            override fun areContentsTheSame(
+                oldItem: FitGroup,
+                newItem: FitGroup,
+            ): Boolean {
+                return oldItem.groupName == newItem.groupName
+            }
+
+        }
+    }
+
+}
