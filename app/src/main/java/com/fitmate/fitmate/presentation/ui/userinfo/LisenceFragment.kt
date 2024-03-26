22198b45591a5a00a2854ace91dee474f296681e
Ganghyungoo <104668071+Ganghyungoo@users.noreply.github.com>
2024-03-26 18:51:54 +0900

    Feat add fit group screen connection
    피트 그룹 화면 및 mock 데이터 연결 완료

--- app/src/main/java/com/fitmate/fitmate/presentation/ui/userinfo/LisenceFragment.kt
+++ app/src/main/java/com/fitmate/fitmate/presentation/ui/userinfo/LisenceFragment.kt
@@ -1,7 +1,23 @@
 package com.fitmate.fitmate.presentation.ui.userinfo
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.fitmate.fitmate.R
+import com.fitmate.fitmate.databinding.FragmentLicenseBinding
+import com.fitmate.fitmate.util.ControlActivityInterface
 
-class LicenseFragment: Fragment(R.layout.fragment_license) {
+class LicenseFragment: Fragment() {
+
+    private lateinit var binding: FragmentLicenseBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentLicenseBinding.bind(view)
+
+        (activity as ControlActivityInterface).goneNavigationBar()
+        binding.toolbarLicense.setupWithNavController(findNavController())
+    }
 }
