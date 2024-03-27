039c4cdec3c9337b1edea41fa83e78c1df07cfd4
Kyungwon Seo <woojugoing@naver.com>
2024-03-27 21:33:42 +0900

    Fix update HomeFragment issue
    홈 화면 아이템이 특정 상황에서 안보이는 이슈 해결

--- app/src/main/java/com/fitmate/fitmate/presentation/ui/home/HomeFragment.kt
+++ app/src/main/java/com/fitmate/fitmate/presentation/ui/home/HomeFragment.kt
@@ -3,50 +3,48 @@ package com.fitmate.fitmate.presentation.ui.home
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.fitmate.fitmate.MainActivity
 import com.fitmate.fitmate.R
 import com.fitmate.fitmate.databinding.FragmentHomeBinding
-import com.fitmate.fitmate.util.ControlActivityInterface
 import com.google.android.material.tabs.TabLayoutMediator
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
 
     private lateinit var binding: FragmentHomeBinding
-    private val tabName = arrayOf("홈", "카테고리")
-    private val fragmentList = mutableListOf<Fragment>()
-    private var tabLayoutMediator: TabLayoutMediator? = null
+
+    private val homeMainFragment = HomeMainFragment()
+    private val homeCategoryFragment = HomeCategoryFragment()
+    private val fragments = listOf(homeMainFragment, homeCategoryFragment)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentHomeBinding.bind(view)
+        (activity as MainActivity).viewNavigationBar()
+        setupViewPagerAndTabs()
+    }
 
-        (activity as ControlActivityInterface).viewNavigationBar()
+    private fun setupViewPagerAndTabs() {
+        val adapter = ViewPagerAdapter(childFragmentManager, viewLifecycleOwner.lifecycle, fragments)
+        binding.pager2Main.adapter = adapter
 
-        fragmentList.apply {
-            if(isEmpty()) {
-                add(HomeMainFragment())
-                add(HomeCategoryFragment())
+        TabLayoutMediator(binding.tabs, binding.pager2Main) { tab, position ->
+            tab.text = if (fragments[position] is HomeMainFragment) {
+                "홈"
+            } else {
+                "카테고리"
             }
-        }
-
-        activity?.let { fragmentActivity ->
-            binding.pager2Main.isUserInputEnabled = false
-            binding.pager2Main.adapter = TabAdapterClass(fragmentActivity)
-            tabLayoutMediator?.detach()
-            tabLayoutMediator = TabLayoutMediator(binding.tabs, binding.pager2Main) { tab, position -> tab.text = tabName[position] }.also { it.attach() }
-        }
+        }.attach()
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        tabLayoutMediator?.detach()
-        tabLayoutMediator = null
-    }
-
-    inner class TabAdapterClass(fragmentActivity: FragmentActivity) :
-        FragmentStateAdapter(fragmentActivity) {
-        override fun getItemCount(): Int = fragmentList.size
-        override fun createFragment(position: Int): Fragment = fragmentList[position]
+    inner class ViewPagerAdapter(
+        fragmentManager: FragmentManager,
+        lifecycle: Lifecycle,
+        private val fragments: List<Fragment>
+    ) : FragmentStateAdapter(fragmentManager, lifecycle) {
+        override fun getItemCount(): Int = fragments.size
+        override fun createFragment(position: Int): Fragment = fragments[position]
     }
 }
