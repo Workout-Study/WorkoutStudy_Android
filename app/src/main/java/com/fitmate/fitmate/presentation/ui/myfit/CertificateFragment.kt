b7ccd32fed909ed6dfad2ba3d2e916916e2d10dc
Ganghyungoo <104668071+Ganghyungoo@users.noreply.github.com>
2024-03-27 13:49:27 +0900

    Feat fix my_fit, chat screeen bottom navi visible
    마이 피트 및 피트 그룹(chat) 화면 바텀 네비게이션 visible 설정 완료

--- app/src/main/java/com/fitmate/fitmate/presentation/ui/myfit/CertificateFragment.kt
+++ app/src/main/java/com/fitmate/fitmate/presentation/ui/myfit/CertificateFragment.kt
@@ -7,16 +7,21 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
+import com.fitmate.fitmate.MainActivity
 import com.fitmate.fitmate.databinding.FragmentCertificateBinding
+import com.fitmate.fitmate.util.ControlActivityInterface
 
 class CertificateFragment: Fragment() {
     private lateinit var binding: FragmentCertificateBinding
+    private lateinit var controlActivityInterface: ControlActivityInterface
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentCertificateBinding.inflate(layoutInflater)
+        controlActivityInterface = activity as MainActivity
+        controlActivityInterface.goneNavigationBar()
         return binding.root
     }
 
