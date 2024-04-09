24dd9b4fcf3dcef27b0adb2fe8d3d2ae7921123e
Kyungwon Seo <woojugoing@naver.com>
2024-04-09 10:42:35 +0900

    Fix update REST API to chat send
    채팅 전송 관련 로직 마무리
    서버에서 전달 후 처리 작업 이행 예정

--- app/src/main/java/com/fitmate/fitmate/data/model/dto/NetworkDto.kt
+++ app/src/main/java/com/fitmate/fitmate/data/model/dto/NetworkDto.kt
@@ -2,7 +2,12 @@ package com.fitmate.fitmate.data.model.dto
 
 import com.google.gson.annotations.SerializedName
 
-
-data class FitGroupResponse(
-    val items: List<FitGroup>
+data class FitGroup(
+    @SerializedName("ID") val id: Int,
+    @SerializedName("FitGroupName") val fitGroupName: String,
+    @SerializedName("MaxFitMate") val maxFitMate: Int,
+    @SerializedName("CreatedAt") val createdAt: String,
+    @SerializedName("CreatedBy") val createdBy: String,
+    @SerializedName("UpdatedAt") val updatedAt: String,
+    @SerializedName("UpdatedBy") val updatedBy: String
 )
