51431bb01ca3913894ad618b4f158453a7928cc1
Ganghyungoo <104668071+Ganghyungoo@users.noreply.github.com>
2024-04-15 22:48:15 +0900

    Feat add setting view visible and myfit progress animation
    my fit화면 빈 데이터 가이드 뷰 visible 설정 및 progress 애니메이션 설정 완료

--- app/src/main/java/com/fitmate/fitmate/util/BindingAdapter.kt
+++ app/src/main/java/com/fitmate/fitmate/util/BindingAdapter.kt
@@ -2,6 +2,7 @@ package com.fitmate.fitmate.util
 
 import android.content.Context
 import android.net.Uri
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.ImageView
@@ -9,8 +10,13 @@ import android.widget.LinearLayout
 import androidx.cardview.widget.CardView
 import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.findViewTreeLifecycleOwner
 import com.bumptech.glide.Glide
+import com.facebook.shimmer.ShimmerFrameLayout
 import com.fitmate.fitmate.R
+import com.fitmate.fitmate.domain.model.FitProgressItem
 import com.fitmate.fitmate.presentation.viewmodel.CertificateState
 import java.net.URI
 
@@ -23,13 +29,14 @@ fun ImageView.setImageByUri(image: String) {
 }
 
 @BindingAdapter("url")
-fun ImageView.setImageByUrl(imageUrl:String) {
+fun ImageView.setImageByUrl(imageUrl: String) {
     Glide.with(this)
         .load(imageUrl)
         .circleCrop()
         .into(this)
 }
 
+//인증 화면의 종료 사진 첨부 컨테이너 visible 설정
 @BindingAdapter("certificate_end_image_container_state")
 fun LinearLayout.setContainerVisible(state: CertificateState) {
     when (state) {
@@ -43,6 +50,7 @@ fun LinearLayout.setContainerVisible(state: CertificateState) {
     }
 }
 
+//인증 화면의 시작 사진 첨부 카드뷰 visible 설정
 @BindingAdapter("certificate_end_image_add_state")
 fun CardView.setContainerVisible(state: CertificateState) {
     when (state) {
@@ -56,6 +64,7 @@ fun CardView.setContainerVisible(state: CertificateState) {
     }
 }
 
+//인증 화면의 버튼 상태 설정
 @BindingAdapter("certificate_button_state")
 fun Button.buttonSetState(state: CertificateState) {
     when (state) {
@@ -76,6 +85,7 @@ fun Button.buttonSetState(state: CertificateState) {
     }
 }
 
+//인증 화면의 리셋 버튼 visible 설정
 @BindingAdapter("certificate_reset_button_state")
 fun Button.resetButtonSetState(state: CertificateState) {
     when (state) {
@@ -92,5 +102,32 @@ fun Button.resetButtonSetState(state: CertificateState) {
         }
     }
 }
+
+//my fit 화면의 아직 피트그룹에 가입되어있지 않은지 알려주는 컨테이너 visible 설정
+@BindingAdapter("myfit_group_data_is_empty")
+fun LinearLayout.setVisibleGuideFitGroupEnter(data: List<FitProgressItem>?) {
+    if (data == null){
+        this.visibility = View.GONE
+    }else{
+        if (data.isEmpty()) {
+            this.visibility = View.VISIBLE
+        } else {
+            this.visibility = View.GONE
+        }
+    }
+}
+//my fit 화면의 Shimmer visible 설정
+@BindingAdapter("myfit_group_data_shimmer")
+fun ShimmerFrameLayout.setVisibleShimmer(data: List<FitProgressItem>?) {
+    if (data == null){
+        this.visibility = View.VISIBLE
+    }else {
+        startShimmer()
+        this.visibility = View.GONE
+    }
+}
+
+
+
 
 
