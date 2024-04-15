b5cd7b9c82a8ad61e44d963cb02889a95432575a
Ganghyungoo <104668071+Ganghyungoo@users.noreply.github.com>
2024-04-15 22:48:15 +0900

    Feat add setting view visible and myfit progress animation
    my fit화면 빈 데이터 가이드 뷰 visible 설정 및 progress 애니메이션 설정 완료

--- app/src/main/java/com/fitmate/fitmate/presentation/ui/myfit/list/MyFitGroupProgressViewHolder.kt
+++ app/src/main/java/com/fitmate/fitmate/presentation/ui/myfit/list/MyFitGroupProgressViewHolder.kt
@@ -1,12 +1,22 @@
 package com.fitmate.fitmate.ui.myfit.list
 
+import android.content.Context
+import android.view.animation.AnimationUtils
 import androidx.recyclerview.widget.RecyclerView
+import com.fitmate.fitmate.R
 import com.fitmate.fitmate.databinding.ItemProgressBinding
 import com.fitmate.fitmate.domain.model.FitProgressItem
 
-class MyFitGroupProgressViewHolder(private val binding:ItemProgressBinding): RecyclerView.ViewHolder(binding.root) {
+class MyFitGroupProgressViewHolder(private val binding:ItemProgressBinding, private val context:Context): RecyclerView.ViewHolder(binding.root) {
 
     fun binding(item: FitProgressItem){
+        val animation = AnimationUtils.loadAnimation(context, R.anim.progress_fill_animation)
+        binding.progressBarItemProgress.startAnimation(animation)
+
+/*        val animator = ObjectAnimator.ofInt(binding.progressBarItemProgress, "progress", binding.progressBarItemProgress.progress, item.certificationCount)
+        animator.duration = 500 // 애니메이션 지속 시간 설정 (ms)
+        animator.start()*/
+
         binding.data = item
     }
 }
