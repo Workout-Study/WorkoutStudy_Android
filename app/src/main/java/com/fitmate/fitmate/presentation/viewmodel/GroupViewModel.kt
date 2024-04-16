380807e9bfde861f0ff439552206d5f77782fc9a
Kyungwon Seo <woojugoing@naver.com>
2024-04-16 13:16:39 +0900

    Fix update GroupViewModel file
    기존의
    GroupViewModel - 그룹에 관한 정보를 가져오는 ViewModel
    MyGroupViewModel - 속해있는 채팅방을 들어가기 위한 그룹 정보 ViewModel
    의 두 가지 ViewModel을 통합시켜 하나의 뷰모델로 적용

--- app/src/main/java/com/fitmate/fitmate/presentation/viewmodel/GroupViewModel.kt
+++ app/src/main/java/com/fitmate/fitmate/presentation/viewmodel/GroupViewModel.kt
@@ -5,15 +5,24 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.fitmate.fitmate.data.model.dto.EachFitResponse
+import com.fitmate.fitmate.data.model.dto.FitGroup
 import com.fitmate.fitmate.data.model.dto.GroupDetailResponse
 import com.fitmate.fitmate.data.model.dto.GroupResponse
+import com.fitmate.fitmate.domain.usecase.DBChatUseCase
 import com.fitmate.fitmate.domain.usecase.GroupUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 @HiltViewModel
-class GroupViewModel @Inject constructor(private val groupUseCase: GroupUseCase) : ViewModel() {
+class GroupViewModel @Inject constructor(
+    private val groupUseCase: GroupUseCase,
+    private val dbChatUseCase: DBChatUseCase
+) : ViewModel() {
     private val _fitGroups = MutableLiveData<GroupResponse>()
     val fitGroups: LiveData<GroupResponse> = _fitGroups
 
@@ -22,6 +31,14 @@ class GroupViewModel @Inject constructor(private val groupUseCase: GroupUseCase)
 
     private val _fitGroupVotes = MutableLiveData<EachFitResponse>()
     val fitGroupVotes: LiveData<EachFitResponse> = _fitGroupVotes
+
+    private val _fitGroup = MutableStateFlow<List<FitGroup>>(emptyList())
+    val fitGroup: StateFlow<List<FitGroup>> = _fitGroup
+
+    private val _isLoading = MutableStateFlow(false)
+    val isLoading: StateFlow<Boolean> = _isLoading
+
+    private var isDataLoadedOnce = false
 
     fun getFitGroups(withMaxGroup: Boolean, category: Int, pageNumber: Int, pageSize: Int) {
         viewModelScope.launch {
@@ -41,6 +58,21 @@ class GroupViewModel @Inject constructor(private val groupUseCase: GroupUseCase)
             val response = groupUseCase.eachFitGroupVotes()
             if(response.isSuccessful) {
                 _fitGroupVotes.postValue(response.body())
+            }
+        }
+    }
+
+    fun retrieveFitGroup(fitMateId: Int) {
+        if (isDataLoadedOnce) return
+        viewModelScope.launch {
+            _isLoading.value = true
+            val response = dbChatUseCase.retrieveFitGroup(fitMateId)
+
+            withContext(Dispatchers.Main) {
+                val result = response.body()
+                _fitGroup.value = result!!
+                _isLoading.value = false
+                isDataLoadedOnce = true
             }
         }
     }
