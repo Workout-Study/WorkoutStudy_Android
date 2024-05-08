1e614d81521cdf34e2a7a26939217a02aa693f4a
Ganghyungoo <104668071+Ganghyungoo@users.noreply.github.com>
2024-05-08 17:26:01 +0900

    Feat add issue of empty data my fitgroup at certification screen
    내가 가입된 그룹이 없을때 이슈 대응 완료
    +가입하러 가기 클릭시 이동 구현 예정

--- app/src/main/java/com/fitmate/fitmate/data/source/remote/CertificationTargetGroupService.kt
+++ app/src/main/java/com/fitmate/fitmate/data/source/remote/CertificationTargetGroupService.kt
@@ -8,6 +8,7 @@ import retrofit2.http.Query
 interface CertificationTargetGroupService {
 
     /*@GET("fit-group-service/groups/filters")*/
-    @GET("group")
+    //@GET("group")
+    @GET("cfeffd3f-44fb-4795-aaa1-9d9ff04d33c9")
     suspend fun getTargetGroupList(@Query("user-id") userId:String): Response<CertificationTargetFitGroupResponseDto>
 }
