94b2b09b7f2cd821b0dec2cbe5594ba961363767
Ganghyungoo <104668071+Ganghyungoo@users.noreply.github.com>
2024-05-22 14:35:23 +0900

    Fix update myfit network url setting for real server network
    my fit 화면 목서버 url에서 실서버 url로 변경

--- app/src/main/java/com/fitmate/fitmate/data/source/remote/MyFitService.kt
+++ app/src/main/java/com/fitmate/fitmate/data/source/remote/MyFitService.kt
@@ -8,13 +8,14 @@ import retrofit2.http.Query
 
 interface MyFitService {
 
-    /*@GET("my-fit-service/my-fits/certifications/progresses")*/
+
     //@GET("a53a2763-eac4-4d66-8d0c-387849b4208f") <-데이터가 있는 화면 보고싶을 때 사용
-    @GET("fe1a3df9-827e-4618-985b-60814f617229") //<-데이터가 없는 화면 보고 싶을 떄 사용
+    //@GET("fe1a3df9-827e-4618-985b-60814f617229") //<-데이터가 없는 화면 보고 싶을 떄 사용
+    @GET("my-fit-service/my-fits/certifications/progresses")
     suspend fun getMyFitProgress(@Query("requestUserId") userId: String): Response<MyFitProgressResponseDto>
 
-    /*@GET("my-fit-service/records/filters")*/
-    @GET("4dec0c6c-1be6-4725-8808-658e4c23ef2b")
+    //@GET("4dec0c6c-1be6-4725-8808-658e4c23ef2b")
+    @GET("my-fit-service/records/filters")
     suspend fun getMyFitRecordHistory(
         @Query("userId") userId: String,
         @Query("recordEndStartDate") startDate: String,
