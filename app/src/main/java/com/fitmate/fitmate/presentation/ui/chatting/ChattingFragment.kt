f87e87aa7a5805c70ab1f37ddbfc57297127ed66
Kyungwon Seo <woojugoing@naver.com>
2024-07-13 14:06:10 +0900

    Feat Profile Fragment into User Info
    프로필 변경 추가 추후 elastic ip 변경 후 수정예정

--- app/src/main/java/com/fitmate/fitmate/presentation/ui/chatting/ChattingFragment.kt
+++ app/src/main/java/com/fitmate/fitmate/presentation/ui/chatting/ChattingFragment.kt
@@ -8,7 +8,6 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.view.inputmethod.InputMethodManager
-import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
