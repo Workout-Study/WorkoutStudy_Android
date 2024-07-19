21e2a53213e699731a2840965f57ecb60595d244
Ganghyungoo <104668071+Ganghyungoo@users.noreply.github.com>
2024-07-20 00:45:37 +0900

    Feat add prepare point networking with paging
    포인트 정보 통신 로직 구현 완료(+paging)

---
+++ app/src/main/java/com/fitmate/fitmate/domain/repository/PointRepository.kt
@@ -0,0 +1,22 @@
+package com.fitmate.fitmate.domain.repository
+
+import androidx.paging.PagingData
+import com.fitmate.fitmate.data.model.dto.PointDto
+import com.fitmate.fitmate.data.model.dto.PointHistoryContentDto
+import com.fitmate.fitmate.data.source.remote.PointService
+import kotlinx.coroutines.flow.Flow
+import retrofit2.Response
+
+interface PointRepository {
+    suspend fun getPointInfo(pointOwnerId: Int, pointOwnerType: String): Response<PointDto>
+
+    suspend fun getPointHistory(
+        withMaxGroup: Int,
+        pointOwnerType: String,
+        historyStartDate: String,
+        historyEndDate: String,
+        pageNumber: Int,
+        pageSize: Int,
+        tradeType: String,
+    ): Flow<PagingData<PointHistoryContentDto>>
+}
